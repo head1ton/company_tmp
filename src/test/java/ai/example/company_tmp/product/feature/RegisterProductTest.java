abdf79f15d600579cbe7aaa903528fbd53bd0d3a
head1ton <head1ton@gmail.com>
2023-11-22 14:45:05 +0900

    수동 빌드 생성

--- src/test/java/ai/example/company_tmp/product/feature/RegisterProductTest.java
+++ src/test/java/ai/example/company_tmp/product/feature/RegisterProductTest.java
@@ -3,20 +3,11 @@ package ai.example.company_tmp.product.feature;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import ai.example.company_tmp.common.ApiTest;
-import ai.example.company_tmp.product.domain.Category;
 import ai.example.company_tmp.product.domain.ProductRepository;
-import ai.example.company_tmp.product.domain.TemperatureZone;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import org.junit.jupiter.api.BeforeEach;
+import ai.example.company_tmp.product.feature.api.RegisterProductApi;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 class RegisterProductTest extends ApiTest {
 
@@ -26,41 +17,7 @@ class RegisterProductTest extends ApiTest {
     @Test
     @DisplayName("상품 등록")
     void registerProduct() {
-        Long weightInGrams = 1000L;
-        Long widthInMillimeters = 100L;
-        Long heightInMillimeters = 100L;
-        Long lengthInMillimeters = 100L;
-        String name = "name";
-        String code = "code";
-        String description = "description";
-        String brand = "brand";
-        String maker = "maker";
-        String origin = "origin";
-        Category category = Category.ELECTRONICS;
-        TemperatureZone temperatureZone = TemperatureZone.ROOM_TEMPERATURE;
-        RegisterProduct.Request request = new RegisterProduct.Request(
-            name,
-            code,
-            description,
-            brand,
-            maker,
-            origin,
-            category,
-            temperatureZone,
-            weightInGrams,
-            widthInMillimeters,
-            heightInMillimeters,
-            lengthInMillimeters
-        );
-//        registerProduct.request(request);
-
-        RestAssured.given().log().all()
-                   .contentType(ContentType.JSON)
-                   .body(request)
-                   .when()
-                   .post("/products")
-                   .then().log().all()
-                   .statusCode(HttpStatus.CREATED.value());
+        new RegisterProductApi().request();
 
         assertThat(productRepository.findAll()).hasSize(1);
     }
---
+++ src/test/java/ai/example/company_tmp/product/feature/api/RegisterProductApi.java
@@ -0,0 +1,110 @@
+package ai.example.company_tmp.product.feature.api;
+
+import ai.example.company_tmp.product.domain.Category;
+import ai.example.company_tmp.product.domain.TemperatureZone;
+import ai.example.company_tmp.product.feature.RegisterProduct.Request;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.springframework.http.HttpStatus;
+
+public class RegisterProductApi {
+
+    private String name = "name";
+    private String code = "code";
+    private String description = "description";
+    private String brand = "brand";
+    private String maker = "maker";
+    private String origin = "origin";
+    private Category category = Category.ELECTRONICS;
+    private TemperatureZone temperatureZone = TemperatureZone.ROOM_TEMPERATURE;
+    private Long weightInGrams = 1000L;
+    private Long widthInMillimeters = 100L;
+    private Long heightInMillimeters = 100L;
+    private Long lengthInMillimeters = 100L;
+
+    public RegisterProductApi name(final String name) {
+        this.name = name;
+        return this;
+    }
+
+    public RegisterProductApi code(final String code) {
+        this.code = code;
+        return this;
+    }
+
+    public RegisterProductApi description(final String description) {
+        this.description = description;
+        return this;
+    }
+
+    public RegisterProductApi brand(final String brand) {
+        this.brand = brand;
+        return this;
+    }
+
+    public RegisterProductApi maker(final String marker) {
+        this.maker = maker;
+        return this;
+    }
+
+    public RegisterProductApi origin(final String origin) {
+        this.origin = origin;
+        return this;
+    }
+
+    public RegisterProductApi category(final Category category) {
+        this.category = category;
+        return this;
+    }
+
+    public RegisterProductApi temperatureZone(final TemperatureZone temperatureZone) {
+        this.temperatureZone = temperatureZone;
+        return this;
+    }
+
+    public RegisterProductApi weightInGrams(final Long weightInGrams) {
+        this.weightInGrams = weightInGrams;
+        return this;
+    }
+
+    public RegisterProductApi widthInMillimeters(final Long widthInMillimeters) {
+        this.widthInMillimeters = widthInMillimeters;
+        return this;
+    }
+
+    public RegisterProductApi heightInMillimeters(final Long heightInMillimeters) {
+        this.heightInMillimeters = heightInMillimeters;
+        return this;
+    }
+
+    public RegisterProductApi lengthInMillimeters(final Long lengthInMillimeters) {
+        this.lengthInMillimeters = lengthInMillimeters;
+        return this;
+    }
+
+    public void request() {
+        Request request = new Request(
+            name,
+            code,
+            description,
+            brand,
+            maker,
+            origin,
+            category,
+            temperatureZone,
+            weightInGrams,
+            widthInMillimeters,
+            heightInMillimeters,
+            lengthInMillimeters
+        );
+//        registerProduct.request(request);
+
+        RestAssured.given().log().all()
+                   .contentType(ContentType.JSON)
+                   .body(request)
+                   .when()
+                   .post("/products")
+                   .then().log().all()
+                   .statusCode(HttpStatus.CREATED.value());
+    }
+}
