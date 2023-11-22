47a0076d990f077644755ce46c246dcf4a852586
head1ton <head1ton@gmail.com>
2023-11-22 10:36:42 +0900

    init Setting

---
+++ src/main/java/ai/example/company_tmp/CompanyTmpApplication.java
@@ -0,0 +1,13 @@
+package ai.example.company_tmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyTmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyTmpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ai/example/company_tmp/CompanyTmpApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.example.company_tmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyTmpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
