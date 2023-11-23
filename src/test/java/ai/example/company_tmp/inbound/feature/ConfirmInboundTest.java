9e8380d4835b1084ad7df3b66154830597b50857
head1ton <head1ton@gmail.com>
2023-11-23 14:45:30 +0900

    4. extract method : getBy

--- src/test/java/ai/example/company_tmp/inbound/feature/ConfirmInboundTest.java
+++ src/test/java/ai/example/company_tmp/inbound/feature/ConfirmInboundTest.java
@@ -1,10 +1,18 @@
 package ai.example.company_tmp.inbound.feature;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import ai.example.company_tmp.inbound.domain.Inbound;
+import ai.example.company_tmp.inbound.domain.InboundItem;
 import ai.example.company_tmp.inbound.domain.InboundRepository;
+import ai.example.company_tmp.inbound.domain.InboundStatus;
+import ai.example.company_tmp.product.fixture.ProductFixture;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 public class ConfirmInboundTest {
 
@@ -13,7 +21,8 @@ public class ConfirmInboundTest {
 
     @BeforeEach
     void setUp() {
-        confirmInbound = new ConfirmInbound();
+        inboundRepository = Mockito.mock(InboundRepository.class);
+        confirmInbound = new ConfirmInbound(inboundRepository);
     }
 
     @Test
@@ -21,15 +30,37 @@ public class ConfirmInboundTest {
     void confirmInbound() {
         final Long inboundNo = 1L;
 
+        final Inbound inbound = new Inbound(
+            "상품명",
+            "상품코드",
+            LocalDateTime.now(),
+            LocalDateTime.now().plusDays(1),
+            List.of(new InboundItem(
+                ProductFixture.aProduct().build(),
+                1L,
+                1500L,
+                "description"
+            ))
+        );
+
+        Mockito.when(inboundRepository.getBy(inboundNo))
+               .thenReturn(inbound);
+
         confirmInbound.request(inboundNo);
 
-//        assertThat(inboundRepository.findById(inboundNo).get().getStatus()).isEqualTo("");
+        assertThat(inbound.getStatus()).isEqualTo(InboundStatus.CONFIRMED);
     }
 
-    private class ConfirmInbound {
+    public class ConfirmInbound {
+
+        private final InboundRepository inboundRepository;
+
+        private ConfirmInbound(final InboundRepository inboundRepository) {
+            this.inboundRepository = inboundRepository;
+        }
 
         public void request(final Long inboundNo) {
-            final Inbound inbound = inboundRepository.findById(inboundNo).orElseThrow();
+            final Inbound inbound = inboundRepository.getBy(inboundNo);
 
             inbound.confirmed();
         }
