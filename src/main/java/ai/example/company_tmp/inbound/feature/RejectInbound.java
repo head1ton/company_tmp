01f036d5247dc5e3d486c15a03215207e663ac3a
head1ton <head1ton@gmail.com>
2023-11-27 21:50:26 +0900

    2. RejectInbound Scenario

--- src/main/java/ai/example/company_tmp/inbound/feature/RejectInbound.java
+++ src/main/java/ai/example/company_tmp/inbound/feature/RejectInbound.java
@@ -2,7 +2,15 @@ package ai.example.company_tmp.inbound.feature;
 
 import ai.example.company_tmp.inbound.domain.Inbound;
 import ai.example.company_tmp.inbound.domain.InboundRepository;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class RejectInbound {
 
     private final InboundRepository inboundRepository;
@@ -11,13 +19,18 @@ public class RejectInbound {
         this.inboundRepository = inboundRepository;
     }
 
-    public void request(final Long inboundNo, final Request request) {
+    @PostMapping("/inbounds/{inboundNo}/reject")
+    @Transactional
+    public void request(
+        @PathVariable final Long inboundNo,
+        @RequestBody @Valid final Request request) {
         final Inbound inbound = inboundRepository.getBy(inboundNo);
 
         inbound.reject(request.rejectionReason);
     }
 
-    public record Request(String rejectionReason) {
-
+    public record Request(
+        @NotBlank(message = "거절 사유는 필수입니다.")
+        String rejectionReason) {
     }
 }
--- src/test/java/ai/example/company_tmp/inbound/feature/RejectInboundTest.java
+++ src/test/java/ai/example/company_tmp/inbound/feature/RejectInboundTest.java
@@ -2,41 +2,30 @@ package ai.example.company_tmp.inbound.feature;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import ai.example.company_tmp.common.ApiTest;
+import ai.example.company_tmp.common.Scenario;
 import ai.example.company_tmp.inbound.domain.Inbound;
-import ai.example.company_tmp.inbound.domain.InboundFixture;
 import ai.example.company_tmp.inbound.domain.InboundRepository;
 import ai.example.company_tmp.inbound.domain.InboundStatus;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 
-class RejectInboundTest {
+class RejectInboundTest extends ApiTest {
 
-    private RejectInbound rejectInbound;
+    @Autowired
     private InboundRepository inboundRepository;
-
-    @BeforeEach
-    void setUp() {
-        inboundRepository = Mockito.mock(InboundRepository.class);
-        rejectInbound = new RejectInbound(inboundRepository);
-    }
 
     @Test
     @DisplayName("입고를 반려/거부")
     void rejectInbound() {
         final Long inboundNo = 1L;
-        final String rejectionReason = "반려 사유";
 
-        final RejectInbound.Request request = new RejectInbound.Request(rejectionReason);
+        Scenario.registerProduct().request()
+                .registerInbound().request()
+                .rejectInbound().request();
 
-        final Inbound inbound = InboundFixture.anInbound().build();
-        Mockito.when(inboundRepository.getBy(inboundNo))
-               .thenReturn(inbound);
-
-        rejectInbound.request(inboundNo, request);
-
+        final Inbound inbound = inboundRepository.getBy(inboundNo);
         assertThat(inbound.getStatus()).isEqualTo(InboundStatus.REJECTED);
     }
-
 }
