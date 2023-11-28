9199d031cb985f747986335dd1f88aef30ac37fb
head1ton <head1ton@gmail.com>
2023-11-28 15:40:33 +0900

    1. Create RegisterLPNTest

---
+++ src/test/java/ai/example/company_tmp/inbound/feature/RegisterLPNTest.java
@@ -0,0 +1,15 @@
+package ai.example.company_tmp.inbound.feature;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class RegisterLPNTest {
+
+    @Test
+    @DisplayName("LPN 등록")
+    void registerLPN() {
+
+    }
+
+
+}
