1f283d56abc29389dadd0883470bb63a174ebd15
head1ton <head1ton@gmail.com>
2023-12-04 11:18:03 +0900

    10. add validate

--- src/test/java/ai/example/company_tmp/location/fixture/RegisterLocationTest.java
+++ src/test/java/ai/example/company_tmp/location/fixture/RegisterLocationTest.java
@@ -61,13 +61,16 @@ class RegisterLocationTest {
             final String locationBarcode,
             final StorageType storageType,
             final UsagePurpose usagePurpose) {
+
             validateConstructor(locationBarcode, storageType, usagePurpose);
+
             this.locationBarcode = locationBarcode;
             this.storageType = storageType;
             this.usagePurpose = usagePurpose;
         }
 
-        private static void validateConstructor(final String locationBarcode,
+        private static void validateConstructor(
+            final String locationBarcode,
             final StorageType storageType,
             final UsagePurpose usagePurpose) {
             Assert.hasText(locationBarcode, "로케이션 바코드는 필수입니다.");
@@ -86,6 +89,12 @@ class RegisterLocationTest {
             String locationBarcode,
             StorageType storageType,
             UsagePurpose usagePurpose) {
+
+            public Request {
+                Assert.hasText(locationBarcode, "로케이션 바코드는 필수입니다.");
+                Assert.notNull(storageType, "보관 타입은 필수입니다.");
+                Assert.notNull(usagePurpose, "보관 목적은 필수입니다.");
+            }
 
             public Location toDomain() {
                 return new Location(locationBarcode, storageType, usagePurpose);
