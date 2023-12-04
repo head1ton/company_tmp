359aa3016950f77b91081e6ef6a0572bac03730c
head1ton <head1ton@gmail.com>
2023-12-04 12:41:13 +0900

    17. RegisterLocation Api 구현

--- src/test/java/ai/example/company_tmp/location/feature/api/RegisterLocationApi.java
+++ src/test/java/ai/example/company_tmp/location/feature/api/RegisterLocationApi.java
@@ -1,8 +1,50 @@
 package ai.example.company_tmp.location.feature.api;
 
+import ai.example.company_tmp.common.Scenario;
+import ai.example.company_tmp.location.domain.StorageType;
+import ai.example.company_tmp.location.domain.UsagePurpose;
+import ai.example.company_tmp.location.fixture.RegisterLocation;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.springframework.http.HttpStatus;
+
 public class RegisterLocationApi {
 
-    public void request() {
-        throw new UnsupportedOperationException("Unsupported request");
+    private String locationBarcode = "A-1-1";
+    private StorageType storageType = StorageType.TOTE;
+    private UsagePurpose usagePurpose = UsagePurpose.MOVE;
+
+    public RegisterLocationApi locationBarcode(final String locationBarcode) {
+        this.locationBarcode = locationBarcode;
+        return this;
+    }
+
+    public RegisterLocationApi storageType(final StorageType storageType) {
+        this.storageType = storageType;
+        return this;
+    }
+
+    public RegisterLocationApi usagePurpose(final UsagePurpose usagePurpose) {
+        this.usagePurpose = usagePurpose;
+        return this;
+    }
+
+    public Scenario request() {
+
+        final RegisterLocation.Request request = new RegisterLocation.Request(
+            locationBarcode,
+            storageType,
+            usagePurpose
+        );
+
+        RestAssured.given().log().all()
+                   .contentType(ContentType.JSON)
+                   .body(request)
+                   .when()
+                   .post("/locations")
+                   .then().log().all()
+                   .statusCode(HttpStatus.CREATED.value());
+
+        return new Scenario();
     }
 }
--- src/test/java/ai/example/company_tmp/location/fixture/RegisterLocationTest.java
+++ src/test/java/ai/example/company_tmp/location/fixture/RegisterLocationTest.java
@@ -5,14 +5,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import ai.example.company_tmp.common.ApiTest;
 import ai.example.company_tmp.common.Scenario;
 import ai.example.company_tmp.location.domain.LocationRepository;
-import ai.example.company_tmp.location.domain.StorageType;
-import ai.example.company_tmp.location.domain.UsagePurpose;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 
 class RegisterLocationTest extends ApiTest {
 
@@ -24,24 +19,6 @@ class RegisterLocationTest extends ApiTest {
     void registerLocation() {
         Scenario
             .registerLocation().request();
-
-        final String locationBarcode = "A-1-1";
-        final StorageType storageType = StorageType.TOTE;
-        final UsagePurpose usagePurpose = UsagePurpose.MOVE;
-
-        final RegisterLocation.Request request = new RegisterLocation.Request(
-            locationBarcode,
-            storageType,
-            usagePurpose
-        );
-
-        RestAssured.given().log().all()
-                   .contentType(ContentType.JSON)
-                   .body(request)
-                   .when()
-                   .post("/locations")
-                   .then().log().all()
-                   .statusCode(HttpStatus.CREATED.value());
 
 //        registerLocation.request(request);
 
