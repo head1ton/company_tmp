4a3ab682d6d8a5c4dfaab313b2eca8e84e1be70c
head1ton <head1ton@gmail.com>
2023-12-04 13:55:44 +0900

    12. add throw message.

--- src/main/java/ai/example/company_tmp/inbound/domain/LPNRepository.java
+++ src/main/java/ai/example/company_tmp/inbound/domain/LPNRepository.java
@@ -11,6 +11,7 @@ public interface LPNRepository extends JpaRepository<LPN, Long> {
     Optional<LPN> findByLPNBarcode(@Param(value = "lpnBarcode") String lpnBarcode);
 
     default LPN getByLPNBarcode(final String lpnBarcode) {
-        return findByLPNBarcode(lpnBarcode).orElseThrow();
+        return findByLPNBarcode(lpnBarcode).orElseThrow(
+            () -> new IllegalArgumentException("해당 LPN을 찾을 수 없습니다. %s".formatted(lpnBarcode)));
     }
 }
