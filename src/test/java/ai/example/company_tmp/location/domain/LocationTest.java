dad3bda859fae80188ac2c2dda788bcba920a29d
head1ton <head1ton@gmail.com>
2023-12-04 14:02:41 +0900

    15. create test case

--- src/test/java/ai/example/company_tmp/location/domain/LocationTest.java
+++ src/test/java/ai/example/company_tmp/location/domain/LocationTest.java
@@ -1,10 +1,28 @@
 package ai.example.company_tmp.location.domain;
 
+import ai.example.company_tmp.inbound.domain.InboundItemFixture;
+import ai.example.company_tmp.inbound.domain.LPN;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class LocationTest {
 
     @Test
+    @DisplayName("로케이션에 LPN을 할당한다.")
     void assignLPN() {
+        final String locationBarcode = "A-1-1";
+        final StorageType storageType = StorageType.TOTE;
+        final UsagePurpose usagePurpose = UsagePurpose.MOVE;
+        final Location location = new Location(locationBarcode, storageType, usagePurpose);
+
+        final String lpnBarcode = "LPN-1";
+        final LocalDateTime expirationAt = LocalDateTime.now().plusDays(1);
+        final LPN lpn = new LPN(lpnBarcode, expirationAt,
+            InboundItemFixture.anInboundItem().build());
+
+        location.assignLPN(lpn);
+
+
     }
 }
