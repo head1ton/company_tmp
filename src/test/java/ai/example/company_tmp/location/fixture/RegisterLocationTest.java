b088de6a43144e493efba7047fe86764e1ef95d9
head1ton <head1ton@gmail.com>
2023-12-04 11:00:41 +0900

    3. Create RegisterLocation Class

--- src/test/java/ai/example/company_tmp/location/fixture/RegisterLocationTest.java
+++ src/test/java/ai/example/company_tmp/location/fixture/RegisterLocationTest.java
@@ -1,13 +1,25 @@
 package ai.example.company_tmp.location.fixture;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RegisterLocationTest {
 
+    private RegisterLocation registerLocation;
+
+    @BeforeEach
+    void setUp() {
+        registerLocation = new RegisterLocation();
+    }
+
     @Test
     @DisplayName("로케이션을 등록한다.")
     void registerLocation() {
+
+    }
+
+    public class RegisterLocation {
 
     }
 }
