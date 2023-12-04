2ffce7b5d9d7e1a1112e5e32eab629b595b715bc
head1ton <head1ton@gmail.com>
2023-12-04 13:13:11 +0900

    6. change signature.

--- src/test/java/ai/example/company_tmp/location/feature/AssignLocationLPNTest.java
+++ src/test/java/ai/example/company_tmp/location/feature/AssignLocationLPNTest.java
@@ -16,8 +16,11 @@ class AssignLocationLPNTest {
     @Test
     @DisplayName("로케이션에 LPN을 할당한다.")
     void assignLocationLPN() {
+        final String locationBarcode = "A-1-1";
+        final String lpnBarcode = "LPN-1";
         final AssignLocationLPN.Request request = new AssignLocationLPN.Request(
-
+            locationBarcode,
+            lpnBarcode
         );
         assignLocationLPN.request(request);
     }
@@ -25,10 +28,10 @@ class AssignLocationLPNTest {
     public class AssignLocationLPN {
 
         public void request(final Request request) {
-            throw new UnsupportedOperationException("Unsupported request");
+
         }
 
-        public record Request() {
+        public record Request(String locationBarcode, String lpnBarcode) {
 
         }
     }
