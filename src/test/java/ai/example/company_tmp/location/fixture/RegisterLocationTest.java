a9cfa4b4df508385047ffbae903e67a6e9663421
head1ton <head1ton@gmail.com>
2023-12-04 11:16:25 +0900

    9. Location Assertions

--- src/test/java/ai/example/company_tmp/location/fixture/RegisterLocationTest.java
+++ src/test/java/ai/example/company_tmp/location/fixture/RegisterLocationTest.java
@@ -3,6 +3,7 @@ package ai.example.company_tmp.location.fixture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 class RegisterLocationTest {
 
@@ -60,9 +61,18 @@ class RegisterLocationTest {
             final String locationBarcode,
             final StorageType storageType,
             final UsagePurpose usagePurpose) {
+            validateConstructor(locationBarcode, storageType, usagePurpose);
             this.locationBarcode = locationBarcode;
             this.storageType = storageType;
             this.usagePurpose = usagePurpose;
+        }
+
+        private static void validateConstructor(final String locationBarcode,
+            final StorageType storageType,
+            final UsagePurpose usagePurpose) {
+            Assert.hasText(locationBarcode, "로케이션 바코드는 필수입니다.");
+            Assert.notNull(storageType, "보관 타입은 필수입니다.");
+            Assert.notNull(usagePurpose, "보관 목적은 필수입니다.");
         }
     }
 
