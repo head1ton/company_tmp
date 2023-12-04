ed2ff8f66c87742fb76ae608b6cdaa2b00fec911
head1ton <head1ton@gmail.com>
2023-12-04 13:10:33 +0900

    5. Create Record : Request

--- src/test/java/ai/example/company_tmp/location/feature/AssignLocationLPNTest.java
+++ src/test/java/ai/example/company_tmp/location/feature/AssignLocationLPNTest.java
@@ -16,13 +16,20 @@ class AssignLocationLPNTest {
     @Test
     @DisplayName("로케이션에 LPN을 할당한다.")
     void assignLocationLPN() {
-        assignLocationLPN.request();
+        final AssignLocationLPN.Request request = new AssignLocationLPN.Request(
+
+        );
+        assignLocationLPN.request(request);
     }
 
     public class AssignLocationLPN {
 
-        public void request() {
+        public void request(final Request request) {
             throw new UnsupportedOperationException("Unsupported request");
+        }
+
+        public record Request() {
+
         }
     }
 }
