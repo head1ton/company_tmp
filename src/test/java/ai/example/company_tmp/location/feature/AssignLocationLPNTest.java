46d33b85189c4e6b1d5b2222f6a11295d9546218
head1ton <head1ton@gmail.com>
2023-12-04 13:06:40 +0900

    2. Create Test Case assignLocationLPN.

--- src/test/java/ai/example/company_tmp/location/feature/AssignLocationLPNTest.java
+++ src/test/java/ai/example/company_tmp/location/feature/AssignLocationLPNTest.java
@@ -1,5 +1,13 @@
 package ai.example.company_tmp.location.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class AssignLocationLPNTest {
 
+    @Test
+    @DisplayName("로케이션에 LPN을 할당한다.")
+    void assignLocationLPN() {
+
+    }
 }
