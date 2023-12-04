1e5212bd3f938570979bf8d494161f8b59f7ef40
head1ton <head1ton@gmail.com>
2023-12-04 14:43:09 +0900

    22. test case 추가

--- src/test/java/ai/example/company_tmp/location/domain/LocationTest.java
+++ src/test/java/ai/example/company_tmp/location/domain/LocationTest.java
@@ -20,7 +20,20 @@ class LocationTest {
 
         assertThat(location.getLocationLPNList()).hasSize(1);
         assertThat(location.getLocationLPNList().get(0).getInventoryQuantity()).isEqualTo(1L);
+    }
 
+    @Test
+    @DisplayName("로케이션에 LPN을 할당한다. 이미 LPN이 존재하면 생성하지 않고 재고만 증가시킨다.")
+    void already_exists_assignLPN() {
+        final Location location = LocationFixture.anLocationFixture().build();
+
+        final LPN lpn = LPNFixture.anLPN().build();
+
+        location.assignLPN(lpn);
+        location.assignLPN(lpn);
+
+        assertThat(location.getLocationLPNList()).hasSize(1);
+        assertThat(location.getLocationLPNList().get(0).getInventoryQuantity()).isEqualTo(2L);
     }
 
 }
