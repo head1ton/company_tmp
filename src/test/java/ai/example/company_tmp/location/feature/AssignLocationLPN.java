e13973b0d3ead288a72da1d34132aa3dcce94c0b
head1ton <head1ton@gmail.com>
2023-12-04 15:03:46 +0900

    28. move inner class to upper level.

---
+++ src/test/java/ai/example/company_tmp/location/feature/AssignLocationLPN.java
@@ -0,0 +1,36 @@
+package ai.example.company_tmp.location.feature;
+
+import ai.example.company_tmp.inbound.domain.LPN;
+import ai.example.company_tmp.inbound.domain.LPNRepository;
+import ai.example.company_tmp.location.domain.Location;
+import ai.example.company_tmp.location.domain.LocationRepository;
+import org.springframework.util.Assert;
+
+public class AssignLocationLPN {
+
+    private final LocationRepository locationRepository;
+    private final LPNRepository lpnRepository;
+
+    public AssignLocationLPN(final LocationRepository locationRepository,
+        final LPNRepository lpnRepository) {
+        this.locationRepository = locationRepository;
+        this.lpnRepository = lpnRepository;
+    }
+
+    public void request(final Request request) {
+        final Location location = locationRepository.getByLocationBarcode(
+            request.locationBarcode);
+
+        final LPN lpn = lpnRepository.getByLPNBarcode(request.lpnBarcode);
+
+        location.assignLPN(lpn);
+    }
+
+    public record Request(String locationBarcode, String lpnBarcode) {
+
+        public Request {
+            Assert.hasText(locationBarcode, "로케이션 바코드는 필수입니다.");
+            Assert.hasText(lpnBarcode, "LPN 바코드는 필수입니다.");
+        }
+    }
+}
--- src/test/java/ai/example/company_tmp/location/feature/AssignLocationLPNTest.java
+++ src/test/java/ai/example/company_tmp/location/feature/AssignLocationLPNTest.java
@@ -1,21 +1,20 @@
 package ai.example.company_tmp.location.feature;
 
-import ai.example.company_tmp.inbound.domain.LPN;
 import ai.example.company_tmp.inbound.domain.LPNRepository;
-import ai.example.company_tmp.location.domain.Location;
 import ai.example.company_tmp.location.domain.LocationRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.util.Assert;
 
 class AssignLocationLPNTest {
 
     private AssignLocationLPN assignLocationLPN;
+    private LocationRepository locationRepository;
+    private LPNRepository lpnRepository;
 
     @BeforeEach
     void setUp() {
-        assignLocationLPN = new AssignLocationLPN();
+        assignLocationLPN = new AssignLocationLPN(locationRepository, lpnRepository);
     }
 
     @Test
@@ -33,26 +32,4 @@ class AssignLocationLPNTest {
 
     }
 
-    public class AssignLocationLPN {
-
-        private LocationRepository locationRepository;
-        private LPNRepository lpnRepository;
-
-        public void request(final Request request) {
-            final Location location = locationRepository.getByLocationBarcode(
-                request.locationBarcode);
-
-            final LPN lpn = lpnRepository.getByLPNBarcode(request.lpnBarcode);
-
-            location.assignLPN(lpn);
-        }
-
-        public record Request(String locationBarcode, String lpnBarcode) {
-
-            public Request {
-                Assert.hasText(locationBarcode, "로케이션 바코드는 필수입니다.");
-                Assert.hasText(lpnBarcode, "LPN 바코드는 필수입니다.");
-            }
-        }
-    }
 }
