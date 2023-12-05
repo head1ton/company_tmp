df24f8a4f4d827c18a2c42b2596d1dc93f4ab226
head1ton <head1ton@gmail.com>
2023-12-05 23:42:54 +0900

    4. create class RegisterPackingMaterial

--- src/test/java/ai/example/company_tmp/outbound/feature/RegisterPackingMaterialTest.java
+++ src/test/java/ai/example/company_tmp/outbound/feature/RegisterPackingMaterialTest.java
@@ -1,13 +1,25 @@
 package ai.example.company_tmp.outbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RegisterPackingMaterialTest {
 
+    private RegisterPackingMaterial registerPackingMaterial;
+
+    @BeforeEach
+    void setUp() {
+        registerPackingMaterial = new RegisterPackingMaterial();
+    }
+
     @Test
     @DisplayName("포장재를 등록한다.")
     void registerPackingMaterial() {
+
+    }
+
+    private class RegisterPackingMaterial {
 
     }
 }
