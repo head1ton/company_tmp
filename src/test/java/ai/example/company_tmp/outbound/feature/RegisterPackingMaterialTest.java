7d8393e38f32ed664a6dc3ae026a8e82f0eaf10c
head1ton <head1ton@gmail.com>
2023-12-05 23:42:02 +0900

    3. create test case

--- src/test/java/ai/example/company_tmp/outbound/feature/RegisterPackingMaterialTest.java
+++ src/test/java/ai/example/company_tmp/outbound/feature/RegisterPackingMaterialTest.java
@@ -1,5 +1,13 @@
 package ai.example.company_tmp.outbound.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class RegisterPackingMaterialTest {
 
+    @Test
+    @DisplayName("포장재를 등록한다.")
+    void registerPackingMaterial() {
+
+    }
 }
