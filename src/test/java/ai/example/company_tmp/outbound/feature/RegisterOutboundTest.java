4679dd30916e4502d0a050fd48a4abde965901c2
head1ton <head1ton@gmail.com>
2023-12-11 16:53:14 +0900

    4. change signature

--- src/test/java/ai/example/company_tmp/outbound/feature/RegisterOutboundTest.java
+++ src/test/java/ai/example/company_tmp/outbound/feature/RegisterOutboundTest.java
@@ -1,8 +1,10 @@
 package ai.example.company_tmp.outbound.feature;
 
+import java.time.LocalDate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 class RegisterOutboundTest {
 
@@ -16,7 +18,14 @@ class RegisterOutboundTest {
     @Test
     @DisplayName("출고를 등록한다.")
     void registerOutbound() {
-        final RegisterOutbound.Request request = new RegisterOutbound.Request();
+        final Long orderNo = 1L;
+        final Boolean isPriorityDelivery = false;
+        final LocalDate desiredDeliveryAt = LocalDate.now();
+        final RegisterOutbound.Request request = new RegisterOutbound.Request(
+            orderNo,
+            isPriorityDelivery,
+            desiredDeliveryAt
+        );
         registerOutbound.request(request);
     }
 
@@ -26,8 +35,16 @@ class RegisterOutboundTest {
             throw new UnsupportedOperationException("Unsupported request");
         }
 
-        public record Request() {
+        public record Request(
+            Long orderNo,
+            Boolean isPriorityDelivery,
+            LocalDate desiredDeliveryAt) {
 
+            public Request {
+                Assert.notNull(orderNo, "주문번호는 필수입니다.");
+                Assert.notNull(isPriorityDelivery, "우선출고여부는 필수입니다.");
+                Assert.notNull(desiredDeliveryAt, "희망출고일은 필수입니다.");
+            }
         }
     }
 }
