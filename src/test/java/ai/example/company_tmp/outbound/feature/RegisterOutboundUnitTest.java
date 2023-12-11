090a290d89c2e15d76b95f000cefb8f691ab5943
Hwan / CTO <head1ton@gmail.com>
2023-12-12 00:32:18 +0900

    Feature/009 2023121122 (#17)
    * 1. 재고 검증 로직 추가
    
    * 2. 로직 이동 서비스 -> 도메인
    
    * 3. introduce parameter
    
    * 4. 코드 정리
    
    * 1. 코드 정리

--- src/test/java/ai/example/company_tmp/outbound/feature/RegisterOutboundUnitTest.java
+++ src/test/java/ai/example/company_tmp/outbound/feature/RegisterOutboundUnitTest.java
@@ -1,6 +1,19 @@
 package ai.example.company_tmp.outbound.feature;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import ai.example.company_tmp.inbound.domain.LPN;
+import ai.example.company_tmp.inbound.domain.LPNFixture;
+import ai.example.company_tmp.location.domain.Inventory;
+import ai.example.company_tmp.location.domain.Location;
+import ai.example.company_tmp.location.domain.LocationFixture;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
 
 class RegisterOutboundUnitTest {
 
@@ -11,5 +24,43 @@ class RegisterOutboundUnitTest {
         registerOutbound = new RegisterOutbound(null, null, null);
     }
 
+    @Test
+    @DisplayName("주문한 상품을 출고할 수 있는 재고가 있는지 확인한다.")
+    void validateInventory() {
+        final Inventory inventory = new Inventory(
+            LocationFixture.aLocation().build(),
+            LPNFixture.anLPN().build()
+        );
+        new Inventories(List.of(inventory), 1L).validateInventory();
+    }
 
+    @Test
+    @DisplayName("주문한 상품이 출고할 수 있는 재고보다 많다면")
+    void fail_over_quantity_validateInventory() {
+        final Inventory inventory = new Inventory(
+            LocationFixture.aLocation().build(),
+            LPNFixture.anLPN().build()
+        );
+
+        assertThatThrownBy(() -> {
+            new Inventories(List.of(inventory), 2L).validateInventory();
+        }).isInstanceOf(IllegalArgumentException.class)
+          .hasMessageContaining("재고가 부족합니다.");
+    }
+
+    @Test
+    @DisplayName("유통기한이 지났으면 재고가 있어도")
+    void expire_validateInventory() {
+        final Location location = LocationFixture.aLocation().build();
+        final LPN lpn = LPNFixture.anLPN().expirationAt(LocalDateTime.now().minusDays(1)).build();
+        final Inventory inventory = new Inventory(
+            location,
+            lpn
+        );
+
+        assertThatThrownBy(() -> {
+            new Inventories(List.of(inventory), 1L).validateInventory();
+        }).isInstanceOf(IllegalArgumentException.class)
+          .hasMessageContaining("재고가 부족합니다.");
+    }
 }
