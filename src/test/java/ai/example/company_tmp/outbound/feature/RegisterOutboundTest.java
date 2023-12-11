85900d1c3bec59111f002ed88e4221e39923fa52
head1ton <head1ton@gmail.com>
2023-12-11 16:49:37 +0900

    3. create record Request & request method

--- src/test/java/ai/example/company_tmp/outbound/feature/RegisterOutboundTest.java
+++ src/test/java/ai/example/company_tmp/outbound/feature/RegisterOutboundTest.java
@@ -1,13 +1,33 @@
 package ai.example.company_tmp.outbound.feature;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RegisterOutboundTest {
 
-    @Test
-    @DisplayName("")
-    void registerOutbound() {
+    private RegisterOutbound registerOutbound;
 
+    @BeforeEach
+    void setUp() {
+        registerOutbound = new RegisterOutbound();
+    }
+
+    @Test
+    @DisplayName("출고를 등록한다.")
+    void registerOutbound() {
+        final RegisterOutbound.Request request = new RegisterOutbound.Request();
+        registerOutbound.request(request);
+    }
+
+    public class RegisterOutbound {
+
+        public void request(final Request request) {
+            throw new UnsupportedOperationException("Unsupported request");
+        }
+
+        public record Request() {
+
+        }
     }
 }
