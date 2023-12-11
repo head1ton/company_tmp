9b0994a90da21dca401e5f6d21a26ad6c86def9e
head1ton <head1ton@gmail.com>
2023-12-11 16:47:29 +0900

    2. create test case registerOutbound

--- src/test/java/ai/example/company_tmp/outbound/feature/RegisterOutboundTest.java
+++ src/test/java/ai/example/company_tmp/outbound/feature/RegisterOutboundTest.java
@@ -1,5 +1,13 @@
 package ai.example.company_tmp.outbound.feature;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class RegisterOutboundTest {
 
+    @Test
+    @DisplayName("")
+    void registerOutbound() {
+
+    }
 }
